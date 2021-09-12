cfde4b0c09e7a3488bb7bb7db488f778e79869e4
digihide <digihide@gmail.com>
2021-09-12 12:46:19 +0900

    change:docker install

--- install.sh
+++ install.sh
@@ -80,22 +80,22 @@ add-apt-repository \
   stable"
 
 # Docker CEのインストール
-apt-get update && apt-get install -y \
-containerd.io=1.2.13-2 \
-docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) \
-docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
+#apt-get update && apt-get install -y \
+#containerd.io=1.2.13-2 \
+#docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) \
+#docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
 
-## docker install(bata)
-#sudo apt-get -y install \
-#    apt-transport-https \
-#    ca-certificates \
-#    curl \
-#    gnupg-agent \
-#    software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#sudo apt-get update
-#sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+## docker install
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 
 # デーモンをセットアップ
